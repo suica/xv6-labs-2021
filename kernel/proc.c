d3a3aa3044e986c286a51db51764ae6f9de4da2e
Sg <suica@users.noreply.github.com>
2022-03-31 01:13:17 +0800

    feat: implement USYSCALL

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,12 @@ found:
     return 0;
   }
 
+  if((p->usc = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -134,6 +140,8 @@ found:
     release(&p->lock);
     return 0;
   }
+
+  p->usc->pid = p->pid;
 
   // Set up new context to start executing at forkret,
   // which returns to user space.
@@ -164,6 +172,9 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  if(p->usc){
+    kfree((void*)p->usc);
+  }
 }
 
 // Create a user page table for a given process,
@@ -196,6 +207,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usc), PTE_R | PTE_U | PTE_W) < 0)
+  {
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +225,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
